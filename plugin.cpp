fd184b0983bb3044468fc9e4a4eb7bceca40e793
Yuhang Zhao <2546789017@qq.com>
2020-03-01 16:34:22 +0800

    Fix.
    Plugin can be loaded even in static mode.
    
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- plugin.cpp
+++ plugin.cpp
@@ -1,8 +1,19 @@
 #include <QQmlEngineExtensionPlugin>
 
+extern void qml_register_types_wangwenx190_QuickMpv();
+
 class MpvDeclarativeWrapper : public QQmlEngineExtensionPlugin {
     Q_OBJECT
+    Q_DISABLE_COPY_MOVE(MpvDeclarativeWrapper)
     Q_PLUGIN_METADATA(IID QQmlEngineExtensionInterface_iid)
+
+public:
+    explicit MpvDeclarativeWrapper(QObject *parent = nullptr)
+        : QQmlEngineExtensionPlugin(parent) {
+        volatile auto registration = &qml_register_types_wangwenx190_QuickMpv;
+        Q_UNUSED(registration)
+    }
+    ~MpvDeclarativeWrapper() override = default;
 };
 
 #include "plugin.moc"
